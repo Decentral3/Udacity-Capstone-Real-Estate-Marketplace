65a7b84b7157343d0158832b17318a86ae1ded11
Decentral3 <98121796+Decentral3@users.noreply.github.com>
2022-04-10 13:39:52 -0400

    Update ERC721Mintable.sol

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
+pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
