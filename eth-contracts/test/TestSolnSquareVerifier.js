ce7a0c8ba94224547513dd5ac72e9b7bcf7447ba
Decentral3 <98121796+Decentral3@users.noreply.github.com>
2022-04-28 17:07:08 -0400

    Update TestSolnSquareVerifier.js

--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,6 +1,6 @@
 // Test if a new solution can be added for contract - SolnSquareVerifier
 const SolnSquareVerifier = artifacts.require('./SolnSquareVerifier');
-const SquareVerifier = artifacts.require('./SquareVerifier');
+const SquareVerifier = artifacts.require('./Verifier');
 // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
 contract('SolnSquareVerifier', accounts => {
     beforeEach(async () => {
